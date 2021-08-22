bd9fc862e1dbc2ee5e25177d821f635d5260860d
Nick Thorpe <nickthorpe71@gmail.com>
2021-08-21 19:56:33 -0500

    sum function test

---
+++ CS/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace LINQ_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(new int[] {1, 2, 3, 4}.Sum());
+            Console.ReadLine();
+        }
+    }
+}
